11ec93e176ea8125fc7776cac6d24120e0e2be60
Lucas Lingle <lucasdaxlingle@gmail.com>
2017-10-31 19:22:53 -0700

    committing everything from completed assignment

---
+++ floydhub_notebook_start_script.sh
@@ -0,0 +1,6 @@
+floyd run \
+  --data lucaslingle/datasets/english-french/1:small_vocab_en \
+  --data lucaslingle/datasets/english-french/1:small_vocab_fr \
+  --mode jupyter \
+  --gpu \
+  --env tensorflow-1.2
